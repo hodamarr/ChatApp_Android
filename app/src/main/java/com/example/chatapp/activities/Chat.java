e888744c0433105bcb50b28813d51b94bd5e84cf
hodamarr <hod110@gmail.com>
2022-06-20 21:54:27 +0300

    commit before pull

--- app/src/main/java/com/example/chatapp/activities/Chat.java
+++ app/src/main/java/com/example/chatapp/activities/Chat.java
@@ -5,9 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.chatapp.R;
+import com.example.chatapp.room.AppDB;
+import com.example.chatapp.room.ContactsDao;
+import com.example.chatapp.room.MsgDao;
 
 public class Chat extends AppCompatActivity {
-
+    private AppDB db;
+    private MsgDao mdao;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
