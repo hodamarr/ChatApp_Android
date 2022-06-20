4f8a529f0a3ba06b8536423066cd535f1b4b2a3b
hodamarr <hod110@gmail.com>
2022-06-20 17:50:26 +0300

    addchat connect to room

--- app/src/main/java/com/example/chatapp/activities/AddChat.java
+++ app/src/main/java/com/example/chatapp/activities/AddChat.java
@@ -9,26 +9,31 @@ import android.widget.EditText;
 import com.example.chatapp.R;
 import com.example.chatapp.databinding.ActivityAddChatBinding;
 import com.example.chatapp.room.AppDB;
+import com.example.chatapp.room.Contacts;
+import com.example.chatapp.room.ContactsDao;
 
 public class AddChat extends AppCompatActivity {
     private ActivityAddChatBinding biding;
     private AppDB db;
-    private PostDao postDao;
+    private ContactsDao cdao;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_chat);
 
         db = Room.databaseBuilder(getApplicationContext(), AppDB.class,"postsDB").build();
-        postDao = db.postDao();
+        cdao = (ContactsDao) db.ContactsDao();
 
         biding.btnAddChat.setOnClickListener(v -> {
             EditText addChatId = findViewById(R.id.edAddChatId);
             EditText addChatname = findViewById(R.id.edAddChatName);
             EditText addChatServer = findViewById(R.id.edAddChatServer);
-            //// work on server here
-            /// then add to local db
-//            Post post = new Post(0, )
+            Contacts contact = new Contacts(addChatname.toString(), addChatId.toString(),
+                    addChatServer.toString());
+            cdao.insert(contact);
+
+            //// add also to db
+            finish();
         });
     }
 }
