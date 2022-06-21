ee6f5f604c9912f5a6745b3f0f01686220d016bf
hodamarr <hod110@gmail.com>
2022-06-21 16:13:09 +0300

    build

--- app/src/main/java/com/example/chatapp/room/MsgDao.java
+++ app/src/main/java/com/example/chatapp/room/MsgDao.java
@@ -1,17 +1,18 @@
 package com.example.chatapp.room;
 
+import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
 import androidx.room.Update;
 
 import java.util.List;
-
+@Dao
 public interface MsgDao {
     @Query("SELECT * FROM Msg")
 List<Contacts> index();
 
-    @Query("SELECT * FROM Msg WHERE who=who")
+    @Query("SELECT * FROM Msg WHERE contactId=contactId")
     Contacts get(String who);
 
     @Insert
