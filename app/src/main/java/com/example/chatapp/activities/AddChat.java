97ca0a237d2b86784958c4ca71016ecab3186c93
ofeka <ofeka@LAPTOP-F7UD0GCF>
2022-06-22 23:06:12 +0300

    adding contact

--- app/src/main/java/com/example/chatapp/activities/AddChat.java
+++ app/src/main/java/com/example/chatapp/activities/AddChat.java
@@ -1,37 +1,39 @@
 package com.example.chatapp.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.room.Room;
-
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.chatapp.R;
 import com.example.chatapp.databinding.ActivityAddChatBinding;
-import com.example.chatapp.room.AppDB;
 import com.example.chatapp.room.Contact;
-import com.example.chatapp.room.ContactDao;
+import com.example.chatapp.viewModels.ContactViewModel;
 
 public class AddChat extends AppCompatActivity {
     private ActivityAddChatBinding biding;
-    private AppDB db;
-    private ContactDao cdao;
+    private ContactViewModel vm;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_chat);
+        vm = new ViewModelProvider(this).get(ContactViewModel.class);
 
-        db = Room.databaseBuilder(getApplicationContext(), AppDB.class,"postsDB").build();
-        cdao = (ContactDao) db.contactDao();
 
-        biding.btnAddChat.setOnClickListener(v -> {
-            EditText addChatId = findViewById(R.id.edAddChatId);
-            EditText addChatname = findViewById(R.id.edAddChatName);
-            EditText addChatServer = findViewById(R.id.edAddChatServer);
-            Contact c = new Contact(addChatname.toString(), addChatId.toString(),
-                    addChatServer.toString());
-            cdao.insert(c);
+        EditText addChatId = findViewById(R.id.edAddChatId);
+        EditText addChatname = findViewById(R.id.edAddChatName);
+        EditText addChatServer = findViewById(R.id.edAddChatServer);
 
+        //Add chat
+        Button add = findViewById(R.id.btnAddChat);
+        add.setOnClickListener(v -> {
+            Contact c = new Contact(addChatname.getText().toString(), addChatId.getText().toString(),
+                    addChatServer.getText().toString());
+            vm.add(c);
             finish();
         });
     }
